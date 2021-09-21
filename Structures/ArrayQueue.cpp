e3c1feb130d725e4b6d973dfa37d38c87b8a8115
Alexandr <31178187+calcq@users.noreply.github.com>
2021-09-21 17:53:42 +0300

    pop

--- Structures/ArrayQueue.cpp
+++ Structures/ArrayQueue.cpp
@@ -7,13 +7,12 @@
 template<class T>
 ArrayQueue<T>::ArrayQueue(int max) : Queue<T>(max){
     _data = new T[max];
+    head = 0;
+    tail = 1;
 }
 
 template<class T>
 int ArrayQueue<T>::push(T t){
-    if(tail == this->max)
-        tail = 0;
-
     if(tail == head){
         return -1;
     }
@@ -21,7 +20,30 @@ int ArrayQueue<T>::push(T t){
     _data[tail] = t;
     tail++;
     this->size++;
+
+    if(tail == this->max)
+        tail = 0;
     return 0;
+}
+
+template<class T>
+T ArrayQueue<T>::pop(){
+    if(this->size > 0){
+        T t = data[head];
+        head++;
+
+        if(head == this->max)
+            head = 0;
+
+        if(head == tail)
+            head--;
+
+        this->size--;
+        
+        return t;
+    }
+
+    return nullptr;
 }
 
 template<class T>
