a36d560598b5cb378a552060815941d8b8e59a46
Alexandr <31178187+calcq@users.noreply.github.com>
2021-10-17 16:34:16 +0300

    init

---
+++ Structures/tree/BinaryTree.h
@@ -0,0 +1,20 @@
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+class BinaryTree{
+private:
+    struct Root{
+        Root* left;
+        Root* right;
+        int data;
+    }
+
+    Root* mainRoot;
+
+public:
+    BinaryTree(){
+
+    }
+};
+
+#endif
