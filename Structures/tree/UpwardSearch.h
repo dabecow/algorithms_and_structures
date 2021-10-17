abbdc46bc2de91c65d15c1dbfd12513174692226
Alexandr <31178187+calcq@users.noreply.github.com>
2021-10-17 17:06:47 +0300

    upsearch template

--- Structures/tree/UpwardSearch.h
+++ Structures/tree/UpwardSearch.h
@@ -8,6 +8,37 @@ struct Node{
     Node* next;
 };
 
+static Node* mainHead;
+static Node* mainTail;
 
+void append(Root* root){
+    Node* node = new Node();
+    node->next = nullptr;
+    node->root = root;
+
+    if(mainHead == nullptr){
+        mainHead = node;
+        mainTail = node;
+    }else{
+        mainTail->next = node;
+        mainTail = node;
+    }
+}
+
+void clear(Node* head){
+    if(head != nullptr){
+        clear(head->next);
+        delete head;
+    }
+
+    if(head == mainHead){
+        mainHead = nullptr;
+        mainTail = nullptr;
+    }
+}
+
+void find(Root* root){
+    
+}
 
 #endif
