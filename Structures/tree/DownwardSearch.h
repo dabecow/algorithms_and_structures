f12aa772a617ef95ecd4e674f4605f924594aa70
Alexandr <31178187+calcq@users.noreply.github.com>
2021-10-18 23:15:19 +0300

    dsearch done

--- Structures/tree/DownwardSearch.h
+++ Structures/tree/DownwardSearch.h
@@ -6,8 +6,41 @@
 
 using namespace std;
 
-static int max = 0;
-static stack<Root*> roots;
+struct RootCell{
+    int depth;
+    Root* root;
+};
+
+static int depth = 0;
+static stack<RootCell*> roots = stack<RootCell*>();
+
+int dfind(Root* root){
+    Root* iroot = root;
+    int ldepth = 0;
+    while(iroot){
+        if(iroot->left && iroot->right){
+            ldepth++;
+            roots.push(new RootCell({ldepth, iroot->right}));
+            iroot = iroot->left;
+        }else if(!iroot->left && !iroot->right){
+            if(ldepth > depth)
+                depth = ldepth;
+            RootCell* cell = roots.top();
+            if(cell){
+                roots.pop();
+                ldepth = cell->depth;
+                iroot = cell->root;
+                delete cell;
+            }
+        }else if(iroot->left || iroot->right){
+            ldepth++;
+            if(iroot->left)
+                iroot = iroot->left;
+            else
+                iroot = iroot->right;
+        }
+    }
+}
 
 
 
